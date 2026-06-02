0d7587ee17cfcc1de89e77bbd0803d60d32ce292
Ankur Kumar Sharma <ankursharmacool1989@gmail.com>
2026-06-02 12:33:33 +0530

    Update Q1.py

--- Module_1/List/Q1.py
+++ Module_1/List/Q1.py
@@ -1,8 +1,15 @@
 # Write a program to find the largest and smallest elements in a list.
 numbers = [10, 5, 25, 8, 30]
 
-largest = max(numbers)
-smallest = min(numbers)
+largest = numbers[0]
+smallest = numbers[0]
 
-print("Largest:", largest)
-print("Smallest:", smallest)
+for i in numbers:
+    if i > largest:
+        largest = i
+
+    if i < smallest:
+        smallest = i
+
+print("Largest =", largest)
+print("Smallest =", smallest)
