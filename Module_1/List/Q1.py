505909e3d878b727e492caa4708d078eee33f34c
Ankur Kumar Sharma <ankursharmacool1989@gmail.com>
2026-06-02 12:31:03 +0530

    Create Q1.py

---
+++ Module_1/List/Q1.py
@@ -0,0 +1,8 @@
+# Write a program to find the largest and smallest elements in a list.
+numbers = [10, 5, 25, 8, 30]
+
+largest = max(numbers)
+smallest = min(numbers)
+
+print("Largest:", largest)
+print("Smallest:", smallest)
