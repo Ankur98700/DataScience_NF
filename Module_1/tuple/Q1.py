574da2ac76a197d6b2d810ee390a5b4544aecec0
Ankur Kumar Sharma <ankursharmacool1989@gmail.com>
2026-06-02 12:40:51 +0530

    Create Q1.py

---
+++ Module_1/tuple/Q1.py
@@ -0,0 +1,9 @@
+# Write a program to check whether an element exists in a tuple.
+t = (10, 20, 30, 40)
+
+element = 20
+
+if element in t:
+    print("Element Found")
+else:
+    print("Element Not Found")
